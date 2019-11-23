1c777bc21f3606682d2d2e86b1e7f29f13c5ca3b
Milan Tenk <tenkmilan@gmail.com>
2019-11-23 11:59:38 +0100

    Demo workspace created

---
+++ projects/my-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MyLibModule } from 'my-lib';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MyLibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
